d64cfb6c2ebf5a15bb90b28bdf06c7e9f377c37c
Marcus <marcusko.work@gmail.com>
2023-10-03 01:26:41 +0800

    Link

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 export default function Navbar() {
     return (
         <nav className="nav">
-            <Link to="/" className="site-title">Crypto Dashboard</Link>
+            <Link to="/pricing" className="site-title">Crypto Dashboard</Link>
             <ul>
                 <CustomLink to="/pricing">Pricing History</CustomLink>
                 <CustomLink to="/portfolio">My Portfolio</CustomLink>
