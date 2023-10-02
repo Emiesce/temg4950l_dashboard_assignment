3cf9aed6887a66601f8e6563eea4b30e277af534
Marcus <106527238+Emiesce@users.noreply.github.com>
2023-10-03 00:24:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 
 import App from './App';
@@ -9,7 +9,7 @@ import App from './App';
 // Hook React App to "root" div
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 )
